0859dc430917b67eeb1257b9af37dbcf2268051b
sharang050796 <sharan.g050796@gmail.com>
2025-03-27 14:42:09 +0530

    Update javaciproject.java

--- javaciproject.java
+++ javaciproject.java
@@ -2,5 +2,6 @@ public class javaciproject {
     public static void main(String[] args) {
         System.out.println("Hi, I'am Sharan.");
         System.out.println("This is my 1st Jenkins CI project");
+        System.out.println("this is my new line to check CI");
     }
 }
