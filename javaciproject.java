199b8f1bb0cf3110e82ece1f3e7ddfd676853481
sharang050796 <sharan.g050796@gmail.com>
2025-03-27 14:27:32 +0530

    1st commit

---
+++ javaciproject.java
@@ -0,0 +1,6 @@
+public class javaciproject {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'am Sharan.");
+        System.out.println("This is my 1st Jenkins CI project");
+    }
+}
